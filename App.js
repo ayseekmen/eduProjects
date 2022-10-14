dab047f00b531388f98dbdbb2340fdbbe4b3e38e
ayseekmen <aysegucluekmen@gmail.com>
2022-10-14 14:55:32 +0300

    Add drawer navigation

---
+++ App.js
@@ -0,0 +1,10 @@
+import 'react-native-gesture-handler';// drawer navigasyon paketi için eklendi.
+
+import App from './src/tabNavigation/Router'
+
+const App2 =()=>{
+    return(
+        <App/>
+    )
+}
+export default App2;
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+import 'react-native-gesture-handler';
+
 import {AppRegistry} from 'react-native';
-import Router from './src/KebapFitness/Router';
+import Router from './App';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => Router);
