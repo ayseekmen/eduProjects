962453b84f6fb32900af78bc664bf2bbfc3797d9
ayseekmen <aysegucluekmen@gmail.com>
2022-10-14 14:50:29 +0300

    Add TabNavigation App

---
+++ src/tabNavigation/Router.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import Favorites from './pages/Favorites';
+import Products from './pages/Products';
+
+const Tab = createBottomTabNavigator();
+
+function App() {
+    return (
+    <NavigationContainer>
+        <Tab.Navigator>
+            <Tab.Screen name="ProductsTab" component={Products} />
+            <Tab.Screen name="FavoritesTab" component={Favorites} />
+        </Tab.Navigator>
+    </NavigationContainer>
+    );
+}
+
+export default App;
