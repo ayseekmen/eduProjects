0d1f98a4ddc0f2db6fac4a3002f9477c12bccf7f
ayseekmen <aysegucluekmen@gmail.com>
2022-10-14 14:49:53 +0300

    Add DrawerNavigation App

---
+++ src/drawerNavigation/Router.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createDrawerNavigator } from '@react-navigation/drawer';
+
+import Favorites from './pages/Favorites';
+import Products from './pages/Products';
+
+const Drawer = createDrawerNavigator();
+
+function App() {
+    return (
+        <NavigationContainer>
+            <Drawer.Navigator
+                screenOptions={{
+                    headerShown: false,
+                }}>
+                <Drawer.Screen name="Products" component={Products} />
+                <Drawer.Screen name="Favorites" component={Favorites} />
+            </Drawer.Navigator>
+        </NavigationContainer>
+    );
+}
+
+export default App;
---
+++ src/drawerNavigation/pages/Favorites.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { SafeAreaView, Text } from "react-native";
+
+function Favorites() {
+    return (
+        <SafeAreaView>
+            <Text>Hello Favorites</Text>
+        </SafeAreaView>
+    );
+}
+
+export default Favorites;
