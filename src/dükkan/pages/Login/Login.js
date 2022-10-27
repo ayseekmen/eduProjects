504f55a54ee0b767f5d19da635406ce8b64ed90b
ayseekmen <aysegucluekmen@gmail.com>
2022-10-27 20:09:44 +0300

    Add login logics

--- src/dükkan/pages/Login/Login.js
+++ src/dükkan/pages/Login/Login.js
@@ -1,14 +1,32 @@
 import React from "react";
-import { SafeAreaView, View, Image } from "react-native";
+import { SafeAreaView, View, Image, Alert } from "react-native";
 import { Formik } from "formik";
+
 import Button from "../../components/Button/Button";
 import Input from "../../components/Input/Input";
-
 import styles from './Login.style';
 
-const Login = () => {
+import Config from "react-native-config";
+import usePost from "../../hooks/usePost";
+
+const Login = ({ navigation }) => {
+    const { data, loading, error, post } = usePost()
+
     function handleLogin(values) {
-        console.log(values);
+        post(Config.API_AUTH_URL + '/login', values);
+    }
+
+    if (error) {
+        Alert.alert('Dükkan', 'Kullanıcı bulunamadı!');
+    }
+
+    if (data) {
+        if (data.status === 'Error') {
+            Alert.alert('Dükkan', 'Kullanıcı bulunamadı!');
+        }
+        else {
+            navigation.navigate('ProductsPage');
+        }
     }
 
     return (
@@ -28,13 +46,16 @@ const Login = () => {
                             placeholder="Kullanıcı adını giriniz..."
                             value={values.username}
                             onType={handleChange('username')}
+                            iconName='account'
                         />
                         <Input
                             placeholder="Şifrenizi giriniz..."
                             value={values.password}
                             onType={handleChange('password')}
+                            iconName='key'
+                            isSecure
                         />
-                        <Button text="Giriş Yap" onPress={handleSubmit} />
+                        <Button text="Giriş Yap" onPress={handleSubmit} loading={loading} />
                     </View>
                 )}
             </Formik>
