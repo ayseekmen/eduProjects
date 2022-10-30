5fc94a219dd339be9692f26466a5fe3d6b777e68
ayseekmen <aysegucluekmen@gmail.com>
2022-10-30 23:55:58 +0300

    Add favorites screen to kodwork

---
+++ src/kodwork/pages/Favorites.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { SafeAreaView, Text } from "react-native";
+
+function Favorites() {
+    return (
+        <SafeAreaView>
+            <Text>Hello</Text>
+        </SafeAreaView>
+    );
+}
+ export default Favorites;
