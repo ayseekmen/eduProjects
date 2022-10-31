6ef29d3ff28ef6e07ae4066e715a61d5b3b7c3b7
ayseekmen <aysegucluekmen@gmail.com>
2022-10-31 20:53:13 +0300

    Update router

--- src/kodwork/Router.js
+++ src/kodwork/Router.js
@@ -1,24 +1,46 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 
 import Jobs from './pages/Jobs';
 import JobsDetail from './pages/JobsDetail';
 import Favorites from './pages/Favorites'
 
+
 const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
 
-function Router() {
-
+const RouterStack = () => {
     return (
-        <NavigationContainer>
-            <Stack.Navigator>
-                    <Stack.Screen name="Jobs" component={Jobs} />
-                    <Stack.Screen name="JobsDetailScreen" component={JobsDetail} />
-                    <Stack.Screen name="FavoritesScreen" component={Favorites} />
-            </Stack.Navigator>
-        </NavigationContainer>
+        <Stack.Navigator screenOptions={{
+            headerShown: false,
+        }}>
+            <Stack.Screen name="JobsScreen" component={Jobs} />
+            <Stack.Screen name="JobsDetailScreen" component={JobsDetail} />
+        </Stack.Navigator>
     );
 };
+
+const RouterDrawer = () => {
+    return (
+        <Stack.Navigator screenOptions={{
+            headerShown: false,
+        }}>
+            <Stack.Screen name="FavoritedJobsScreen" component={Favorites} />
+        </Stack.Navigator>
+    );
+};
+
+function Router() {
+    return (
+        <NavigationContainer>
+            <Drawer.Navigator>
+                <Drawer.Screen options={{ drawerActiveTintColor: '#ee534f' }} name="Jobs" component={RouterStack} />
+                <Drawer.Screen options={{ drawerActiveTintColor: '#ee534f' }} name="Favorited Jobs" component={RouterDrawer} />
+            </Drawer.Navigator>
+        </NavigationContainer >
+    )
+}
 
 export default Router;
