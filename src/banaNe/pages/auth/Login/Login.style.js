e45d4ef194d9bb2101d8d2591c887073a9e6c14f
ayseekmen <aysegucluekmen@gmail.com>
2022-11-06 18:08:10 +0300

    Update login page

--- src/banaNe/pages/auth/Login/Login.style.js
+++ src/banaNe/pages/auth/Login/Login.style.js
@@ -5,18 +5,7 @@ import colors from '../../../styles/colors';
 export default StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: 'white'
-    },
-    logo: {
-        width : Dimensions.get('window').width * 0.9,
-        height: Dimensions.get('window').height / 3,
-        resizeMode: 'contain',
-        alignSelf: 'center',
-        tintColor: 'white',
-    },
-    logo_container: {
-        flex: 1,
-        justifyContent: 'center',
+        backgroundColor: 'white',
     },
     body_container: {
         flex: 1,
@@ -24,6 +13,6 @@ export default StyleSheet.create({
     header : {
         color: colors.darkgreen,
         margin: 5,
-        fontSize: 160,
+        fontSize: Platform.OS === 'android' ? 120 : 160,
     }
 });
