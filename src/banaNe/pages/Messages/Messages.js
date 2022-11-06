765737d63c2c676be11ffff2135c9eb59e29f9c7
ayseekmen <aysegucluekmen@gmail.com>
2022-11-06 17:02:19 +0300

    Update message page

--- src/banaNe/pages/Messages/Messages.js
+++ src/banaNe/pages/Messages/Messages.js
@@ -1,33 +1,84 @@
-import React, { useState } from 'react';
-import {SafeAreaView, Text} from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { SafeAreaView, FlatList } from 'react-native';
 import FloatingButton from '../../components/FloatingButton';
 import ContentInputModal from '../../components/modal/ContentInputModal';
+import MessagesCard from '../../components/card/MessageCard'
+
+import auth from '@react-native-firebase/auth';
+import database from "@react-native-firebase/database";
 
 import styles from './Messages.style';
+import parseContentData from '../../utils/parseContentData';
 
 const Messages = () => {
   const [inputModalVisible, setInputModalVisible] = useState(false)
+  const [contentList, setContentList] = useState([]);
+
+  useEffect(() => {
+    database()
+      .ref('messages/')
+      .on('value', snapshot => {
+        const contentData = snapshot.val();
+
+        const parsedData = parseContentData(contentData || {});
+        setContentList(parsedData);
+      })
+  }, [])
 
   function handleInputToggle() {
     setInputModalVisible(!inputModalVisible);
   }
 
-  function handleSendContent(content){
+  function handleSendContent(content) {
     handleInputToggle();
-    console.log(content);
+    sendContent(content);
   }
 
-  return(
+  function sendContent(content) {
+    const userMail = auth().currentUser.email;
+
+    const contentObject = {
+      text: content,
+      username: userMail.split('@')[0],
+      date: new Date().toISOString(),
+      dislike: 0,
+    }
+
+    database().ref('messages/').push(contentObject);
+  }
+
+  function handleBanane(item) {
+    database()
+    .ref(`messages/${item.id}/`)
+    .update({dislike: item.dislike + 1});
+  }
+
+  const renderContent = ({ item }) => {
+
+    
+    return(
+
+      <MessagesCard message={item} onBanane={() => handleBanane(item)} />
+    )
+  }
+  
+
+  return (
     <SafeAreaView style={styles.container}>
+      <FlatList
+        data={contentList}
+        renderItem={renderContent}
+      />
+
       <FloatingButton icon="plus" onPress={handleInputToggle} />
 
       <ContentInputModal
-      visible={inputModalVisible}
-      onClose={handleInputToggle}
-      onSend={handleSendContent}
+        visible={inputModalVisible}
+        onClose={handleInputToggle}
+        onSend={handleSendContent}
       />
     </SafeAreaView>
   );
 };
- 
+
 export default Messages;
