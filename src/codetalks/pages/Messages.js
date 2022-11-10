d3941e3ad6c74274ed910e5f581b15f8382d86ad
ayseekmen <aysegucluekmen@gmail.com>
2022-11-10 22:35:06 +0300

    Add messages page

---
+++ src/codetalks/pages/Messages.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { SafeAreaView, Text } from "react-native";
+
+function Messages() {
+    return (
+        <SafeAreaView>
+            <Text>Hello Messages</Text>
+        </SafeAreaView>
+    );
+}
+
+export default Messages;
