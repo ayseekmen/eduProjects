a1ac0578e45d17f15274d0835d66a1ed63da8f2f
ayseekmen <aysegucluekmen@gmail.com>
2022-11-12 01:23:36 +0300

    Update messages page

--- src/codetalks/pages/Messages.js
+++ src/codetalks/pages/Messages.js
@@ -1,10 +1,68 @@
-import React from "react";
-import { SafeAreaView, Text } from "react-native";
+import React, { useEffect, useState } from "react";
+import { Button, FlatList, SafeAreaView, Text, TextInput, View } from "react-native";
+import database from "@react-native-firebase/database";
+import auth from '@react-native-firebase/auth';
+import parseContentData from "../../banaNe/utils/parseContentData";
 
-function Messages() {
+function Messages({ route }) {
+
+    const roomName = route.params.item
+
+    const [message, setMessage] = useState("")
+    const [allMessage, setAllMessage] = useState([])
+
+    const sendMessage = () => {
+        const userMail = auth().currentUser.email;
+
+        const reference = database().ref(`${roomName}/`);
+
+        reference.push({
+            username: userMail.split('@')[0],
+            date: new Date().toISOString(),
+            message: message
+        })
+    };
+
+
+    useEffect(() => {
+        // database'i dinler. Bir kere çalışması yeterlidir.
+        const listenDB = () => {
+            const reference = database().ref(`${roomName}`);
+            reference.on('value', snapshot => {
+                const contentData = snapshot.val();
+
+                const parsedData = parseContentData(contentData || {});
+                setAllMessage(parsedData);
+            });
+        };
+
+        listenDB()
+    }, [])
     return (
         <SafeAreaView>
-            <Text>Hello Messages</Text>
+
+            <View style={{ height: 200, width: 400, backgroundColor: "grey" }}>
+
+                <TextInput
+                    placeholder="Oda adı..."
+                    value={message}
+                    onChangeText={setMessage}
+                    multiline
+                />
+
+            </View>
+
+            <FlatList
+            inverted={true}
+                data={allMessage}
+                renderItem={({ item }) => {
+                    console.log(item.message)
+                    return (
+                        <Text>{item.message}</Text>
+                    )
+                }}
+            />
+            <Button title="gönder" onPress={sendMessage} />
         </SafeAreaView>
     );
 }
