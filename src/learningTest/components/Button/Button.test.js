6c0d01570d7ce61b6885b7638f68c5fd0b5e6fb9
ayseekmen <aysegucluekmen@gmail.com>
2022-11-15 00:11:09 +0300

    Update button test

--- src/learningTest/components/Button/Button.test.js
+++ src/learningTest/components/Button/Button.test.js
@@ -1,12 +1,26 @@
 import React from "react";
-import { render } from '@testing-library/react-native'
+import { render, fireEvent } from '@testing-library/react-native'
 import Button from "./Button";
 
 test('should match with snapshot', () => {
     const comp = render(<Button />);
     expect(comp).toMatchSnapshot();
+});
+
+test('should render title correctly', () => {
+    const testTitle = 'test';
+    const comp = render(<Button title={testTitle} />);
+
+    const buttonText = comp.getByTestId('button-title').children[0];
+    expect(buttonText).toBe(testTitle);
 })
 
-test('check number', () => {
-    expect(5 + 6).toBe(9)
+test('should trigerr onPress', () => {
+    const mockFunction = jest.fn()
+    const comp = render(<Button onClick={mockFunction} />);
+
+    const buttonTouchable = comp.getByTestId('button-touchable')
+    fireEvent(buttonTouchable, 'press')
+
+    expect(mockFunction).toBeCalled()
 })
