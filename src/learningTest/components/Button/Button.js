a818cf2aec1f1438de72e2534389910301fecd7e
ayseekmen <aysegucluekmen@gmail.com>
2022-11-15 16:28:57 +0300

    Update button component

--- src/learningTest/components/Button/Button.js
+++ src/learningTest/components/Button/Button.js
@@ -3,13 +3,13 @@ import { TouchableOpacity, Text, View } from "react-native";
 
 import styles from './Button.style'
 
-const Button = ({ title, onClick }) => {
+const Button = ({ title, onClick, theme = 'primary' }) => {
     return (
         <TouchableOpacity
             testID="button-touchable"
-            style={styles.container}
+            style={styles[theme].container}
             onPress={onClick}>
-            <Text testID="button-title" style={styles.title}>
+            <Text testID="button-title" style={styles[theme].title}>
                 {title}
             </Text>
         </TouchableOpacity>
