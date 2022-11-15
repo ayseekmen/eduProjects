ab46f374583710ee8de0462f76904cb2c317d3be
ayseekmen <aysegucluekmen@gmail.com>
2022-11-15 22:21:07 +0300

    Add app test

--- src/learningTest/App.js
+++ src/learningTest/App.js
@@ -1,14 +1,53 @@
 import React from "react";
-import { SafeAreaView, Text } from "react-native";
+import { SafeAreaView, TextInput, FlatList, Text, StyleSheet } from "react-native";
 import Button from "./components/Button";
 
 const App = () => {
-    return(
-        <SafeAreaView>
-            <Text>Testing</Text>
-            <Button title="Selamlar!" />
+    const [text, setText] = React.useState("");
+    const [list, setList] = React.useState([]);
+
+    const renderElements = ({ item }) => <Text>{item}</Text>
+
+    function addToList() {
+        if (!text) {
+            return;
+        }
+
+
+        setList([...list, text])
+    }
+
+
+
+    return (
+        <SafeAreaView style={styles.container} >
+            <FlatList
+                keyExtractor={(_, i) => i.toString()}
+                testID="list" data={list} renderItem={renderElements}
+            />
+
+            <TextInput
+                testID="input-area"
+                placeholder="Add.."
+                style={styles.input}
+                onChangeText={setText}
+            />
+
+            <Button title="Add" onClick={addToList} />
         </SafeAreaView>
     )
 }
 
 export default App;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    input: {
+        backgroundColor: '#e0e0e0',
+        margin: 10,
+        padding: 10,
+        borderRadius: 10,
+    }
+})
