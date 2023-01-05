520d1c03402974c8f8aeabf7cb8d55e50cad3b17
ayseekmen <aysegucluekmen@gmail.com>
2023-01-05 18:03:41 +0300

    Add router learninigUseStateUseEffect

---
+++ src/learninigUseStateUseEffect/Router.js
@@ -0,0 +1,66 @@
+import React, { useEffect, useState } from "react";
+import { SafeAreaView, Text, View, Button, StyleSheet, Touchable } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+
+function App() {
+
+    const alphabet = ["A","B","C","D","E",]
+
+
+    const [index, setIndex] = useState(0);
+
+    function arttırHarf() {
+        setIndex(index +1);
+    }
+
+    function azaltHarf() {
+        setIndex(index-1);
+    }
+
+    return (
+        <SafeAreaView style={styles.container}>
+            <Text style={styles.numberText}>Harf: {alphabet[index]}</Text>
+            <View style={styles.buttons}>
+                <TouchableOpacity
+                    style={styles.button}
+                    onPress={arttırHarf} >
+                    <Text style={styles.buttonText}>+</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                    style={styles.button}
+                    onPress={azaltHarf} >
+                    <Text style={styles.buttonText}>-</Text>
+                </TouchableOpacity>
+            </View>
+        </SafeAreaView>
+    );
+}
+
+export default App;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    numberText: {
+        fontSize: 40,
+        margin: 20
+    },
+    buttons: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        height: 50,
+        paddingHorizontal: 50,
+        marginVertical: 50
+    },
+    button: {
+        height: 40,
+        width: 40,
+        borderWidth: 1,
+        borderColor: 'black'
+    },
+    buttonText: {
+        fontSize: 30,
+        alignSelf: 'center'
+    }
+})
